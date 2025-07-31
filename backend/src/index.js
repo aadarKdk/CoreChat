9e75708b16ab45912fa03ed4358d6ca2b79fecd9
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-07-31 15:23:30 +0545

    API hit from frontend successful

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/User.js';
 import connect from './db/connect.js';
 
@@ -10,7 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+
+
 app.use(express.json());
+
+app.use(cors());
 
 await connect(); // Ensure DB connection before starting (if your environment supports top-level await)
 
