141dfdcd29b40093d9069c41cec7722f06a37e96
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-08-02 23:01:42 +0545

    hashed password for security

--- backend/src/controllers/User.js
+++ backend/src/controllers/User.js
@@ -1,24 +1,38 @@
 // CoreChat/backend/src/controllers/User.js
 
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 const getAllUsers = async (req, res) => {
     try {
-        const user = await User.find();
-        res.status(200).json(user);
+        const users = await User.find();
+        res.status(200).json(users);
+        
     } catch (error) {
-        console.error("Error feteching users: ", error.message);
-        res.status(500).json({ message: "Failed to fetch users. "});
+        console.log.error("Error feteching users: ", error.message);
+        res.status(500).json({ message: " Failed to fetch users. " });
+        
     }
 };
 
 const registerNewUser = async (req, res) => {
     try {
-        const user = await User.create(req.body);
-        res.status(200).json(user);
+        const existingUser = await User.exists({ email: req.body.email });
+        if(existingUser){
+            return res.status(400).json({ message: "E-mail already exists. Use another email to register. "});
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = await User.create({
+            ...req.body,
+            password: hashedPassword
+        });
+        res.status(201).json(newUser);
+
+
     } catch (error) {
-        console.error("Error adding user: ", error.message);
-        res.status(500).json({ message: "Failed to add user. "});
+        console.error("Error adding user: ", error.message );
+        res.status(500).json({ message: "Failed to add user." });
     }
 };
 
