e4d402e29728a5bb5739c66e9ca1b375bd710931
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-08-05 22:43:38 +0545

    jwt implemented

--- backend/src/routes/User.js
+++ backend/src/routes/User.js
@@ -1,11 +1,12 @@
 // CoreChat/backend/src/routes/User.js
 
 import express from 'express';
-import { getAllUsers, registerNewUser } from '../controllers/User.js';
+import { getAllUsers, registerNewUser, loginUser } from '../controllers/User.js';
 
 const router = express.Router();
 
 router.get('/users', getAllUsers);
 router.post('/register', registerNewUser);
+router.post('/login', loginUser);
 
 export default router;
